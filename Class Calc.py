f57551907a1fa6d2215f891359778860031e9fc2
MrArtemka <kohanv.artem@gmail.com>
2020-04-01 23:27:43 +0300

    commit

--- Class Calc.py
+++ Class Calc.py
@@ -1,19 +1,37 @@
 # 1 Вариант
-# class Cal1:
-#     def Calculator1():
-#         a = int(input())
-#         b = int(input())
-#         what = input()
+class Cal:
+    def __init__(self):
+        self.a = int(input())
+        self.b = int(input())
+        self.what = input()
+    def Calculator(self):
+        if self.what == '+':
+            return self.a + self.b
+        elif self.what == '-':
+            return self.a - self.b
+        elif self.what == '/':
+            return self.a / self.b
+        elif self.what == '*':
+            return self.a * self.b
+        
+C = Cal()
+print(C.Calculator())
 
-#         if what == '+':
-#             return a + b
-#         elif what == '-':
-#             return a - b
-#         elif what == '/':
-#             return a / b
-#         elif what == '*':
-#             return a * b
+# 2 Вариант
 
+class Cal2:
+    def Calculator2(self):
+        a = int(input())
+        b = int(input())
+        what = input()
+        if what == '+':
+            return a + b
+        elif what == '-':
+            return a - b
+        elif what == '/':
+            return a / b
+        elif what == '*':
+            return a * b
 
-
-# print(Cal1.Calculator1())
+C2 = Cal2()
+print(C2.Calculator2())
