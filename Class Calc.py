4fb4fe64694ef89f676c03007d567bd0b16e9026
MrArtemka <kohanv.artem@gmail.com>
2020-04-01 23:18:19 +0300

    commit

---
+++ Class Calc.py
@@ -0,0 +1,19 @@
+# 1 Вариант
+# class Cal1:
+#     def Calculator1():
+#         a = int(input())
+#         b = int(input())
+#         what = input()
+
+#         if what == '+':
+#             return a + b
+#         elif what == '-':
+#             return a - b
+#         elif what == '/':
+#             return a / b
+#         elif what == '*':
+#             return a * b
+
+
+
+# print(Cal1.Calculator1())
